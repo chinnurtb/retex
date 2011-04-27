5bbfcc8306c1340f1a138da4aab394b2486160d5
Jamie Brandon <jamie@scattered-thoughts.net>
2011-04-28 01:17:59 +0800

    Crash on latex timeout instead of reporting

--- src/latex.erl
+++ src/latex.erl
@@ -2,25 +2,25 @@
 
 -export([ast/1, hash/1]).
 
--spec interact(string(), binary()) -> {'ok', Reply :: binary()} | timeout.
+-spec interact(string(), binary()) -> Reply :: binary().
 interact(Command, Request) when is_list(Command) and is_binary(Request) ->
     Port = open_port({spawn, Command},[{packet, 2}, binary]),
     try
 	port_command(Port, Request),
 	receive
-	    {Port, {data, Data}} -> {ok, Data}
+	    {Port, {data, Data}} -> Data
 	after 
-	    1000 -> timeout
+	    1000 -> erlang:error('latex.timeout')
 	end
     after
 	port_close(Port)
     end.
 
--spec ast(binary()) -> {'ok', Ast :: binary()} | timeout.
+-spec ast(binary()) -> Ast :: binary().
 ast(Latex) ->
     interact("./src/latex_ast", Latex).
 
--spec hash(binary()) -> {'ok', Hash :: binary()} | timeout.
+-spec hash(binary()) -> Hash :: binary().
 hash(Latex) ->
     interact("./src/latex_hash", Latex).
     
