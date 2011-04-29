b95d078dbc930ecba905da89acb16acc4d5d7407
Jamie Brandon <jamie@scattered-thoughts.net>
2011-04-29 18:28:40 +0800

    Pull in webmachine

--- src/retex_app.erl
+++ src/retex_app.erl
@@ -1,24 +1,26 @@
+%% @author author <author@example.com>
+%% @copyright YYYY author.
+
+%% @doc Callbacks for the retex application.
+
 -module(retex_app).
+-author('author <author@example.com>').
 
 -behaviour(application).
+-export([start/2,stop/1]).
 
-%% Application callbacks
--export([start/2, stop/1]).
-
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
-
-start(_StartType, _StartArgs) ->
-    crypto:start(),
-    db:ensure_schema(),
-    mnesia:start(),
-    formula:start(),
-    challenge:start(),
-    response:start(),
+%% @spec start(_Type, _StartArgs) -> ServerRet
+%% @doc application start callback for retex.
+start(_Type, _StartArgs) ->
+    ok = db:ensure_schema(),
+    ok = mnesia:start(),
+    ok = formula:start(),
+    ok = challenge:start(),
+    ok = response:start(),
     retex_sup:start_link().
 
+%% @spec stop(_State) -> ServerRet
+%% @doc application stop callback for retex.
 stop(_State) ->
-    mnesia:stop(),
-    crypto:stop(),
+    ok = mnesia:stop(),
     ok.
