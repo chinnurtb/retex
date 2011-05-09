a54d6e90590d07b7237394308801ee07963b2479
Jamie Brandon <jamie@scattered-thoughts.net>
2011-05-09 16:44:07 +0800

    Return correct response headers on post resource creation

--- src/resource_challenge.erl
+++ src/resource_challenge.erl
@@ -3,7 +3,7 @@
 -module(resource_challenge).
 
 % webmachine callbacks
--export([init/1, allowed_methods/2, post_is_create/2, create_path/2, content_types_accepted/2, from_json/2, content_types_provided/2, to_json/2]).
+-export([init/1, allowed_methods/2, post_is_create/2, create_path/2, content_types_accepted/2, from_json/2, finish_request/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 -include("types.hrl").
@@ -19,7 +19,9 @@ post_is_create(ReqData, Context) ->
 
 create_path(ReqData, Context) ->
     Id = id:new(challenge),
-    {binary_to_list(Id), ReqData, Context}.
+    Location = wrq:path(ReqData) ++ "/" ++ binary_to_list(Id),
+    ReqData2 = wrq:set_resp_header("Location", Location, ReqData),
+    {binary_to_list(Id), ReqData2, Context}.
 
 content_types_accepted(ReqData, Context) ->
    {[{"application/json", from_json}], ReqData, Context}.
@@ -35,9 +37,8 @@ from_json(ReqData, _Context) ->
 	    {halt, 400} % bad request
     end.
 
-content_types_provided(ReqData, Context) ->
-   {[{"application/json", to_json}], ReqData, Context}.
-
-to_json(ReqData, Challenge) ->
+finish_request(ReqData, Challenge) ->
+    ReqData2 = wrq:set_resp_header("Content-Type", "application/json", ReqData),
     Json = challenge:to_json(Challenge),
-    {mochijson2:encode(Json), ReqData, Challenge}.
+    ReqData3 = wrq:set_resp_body(mochijson2:encode(Json), ReqData2),
+    {true, ReqData3, Challenge}.
